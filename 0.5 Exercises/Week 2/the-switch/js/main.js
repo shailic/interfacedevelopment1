9db95e08c4281e1bf63f55200e78a8950c1b4d87
shailic <shaili.c@gmail.com>
2016-05-23 16:58:58 -0400

    completed exercise the-switch

--- 0.5 Exercises/Week 2/the-switch/js/main.js
+++ 0.5 Exercises/Week 2/the-switch/js/main.js
@@ -1,23 +1,34 @@
 // 1. When someone hits the button in the switch, remove the current state class (on/off), and apply the opposite class (on/off)
+
 // 2. Swap the colors of the body's background and text by adding or removing the dark/light class
-// 3. Lastly, change the text in box to say "It's so bright in here!" when it's on, and "Hey, who turn off the lights?" when it's off
+
+// 3. Lastly, change the text in box to say "It's so bright in here!" when it's on, and "Hey, who turned off the lights?" when it's off
 
 ///////////////////////////////
 // VARIABLES
 ///////////////////////////////
 
-// 1. USER HITS BUTTON //
-var lights = function() {
+var lightsOn = "It's so bright in here!"
+var lightsOff = "Hey, who turned off the lights?"
 
-  if(class="on"){
-    $(".on").toggleClass(".off");
-  }
-
-};
 
 ///////////////////////////////
-// OUTPUTS
+// OUTPUT
 ///////////////////////////////
 
-// 1. On -> Off
-$(".switch").on("click", lights);
+$(document).ready(function(){
+
+  $(".switch").click(function(){
+    $("button").toggleClass("off");
+
+    $("body").toggleClass("dark");
+
+    if ( $("button").hasClass("off") ) {
+      $(".status").text(lightsOff);
+    }
+    else {
+      $(".status").text(lightsOn);
+    }
+  });
+
+});
